0d8c30f7dce0b947cd19deb04d9658325e65f1ba
Gary Button <garyhbutton@gmail.com>
2019-04-02 21:27:14 -0700

    Fix the dev console

--- electron/public/electron.js
+++ electron/public/electron.js
@@ -16,7 +16,7 @@ function createWindow () {
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, 'index.html')}`);
 
   // Open the DevTools.
-  if (true) {
+  if (isDev) {
     mainWindow.webContents.openDevTools()
   }
 
