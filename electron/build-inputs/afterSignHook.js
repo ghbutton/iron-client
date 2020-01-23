c47ad540f1baf326c5bec9a3989fa684e4a7a202
Gary Button <garyhbutton@gmail.com>
2020-01-23 08:56:25 -0800

    Fix after sign hook

--- electron/build-inputs/afterSignHook.js
+++ electron/build-inputs/afterSignHook.js
@@ -5,33 +5,35 @@ const path = require('path');
 var electron_notarize = require('electron-notarize');
 
 module.exports = async function (params) {
-    // Only notarize the app on Mac OS only.
-    if (params.electronPlatformName !== 'darwin') {
-        return;
-    }
-    console.log('afterSign hook triggered');
+  // Only notarize the app on Mac OS only.
+  if (params.electronPlatformName !== 'darwin') {
+    return;
+  }
+  console.log('afterSign hook triggered');
 
-    // Same appId in electron-builder.
-    let appId = 'com.ironnotice.messenger'
+  // Same appId in electron-builder.
+  let appId = 'com.ironnotice.messenger'
 
-    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
-    if (!fs.existsSync(appPath)) {
-        throw new Error(`Cannot find application at: ${appPath}`);
-    }
+  let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`);
+  }
 
-    console.log(`Notarizing ${appId} found at ${appPath}`);
+  console.log(`Notarizing ${appId} found at ${appPath}`);
 
-    try {
-        await electron_notarize.notarize({
-            appBundleId: appId,
-            appPath: appPath,
-            appleId: process.env.APPLE_ID,
-            appleIdPassword: process.env.APPLE_ID_PASSWORD,
-            ascProvider: process.env.APPLE_ASC_PROVIDER,
-        });
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    await electron_notarize.notarize({
+      appBundleId: appId,
+      appPath: appPath,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASSWORD,
+      ascProvider: process.env.APPLE_ASC_PROVIDER,
+    });
+  } catch (error) {
+    console.log("Notarization error");
+    console.error(error);
+    throw error
+  }
 
-    console.log(`Done notarizing ${appId}`);
+  console.log(`Done notarizing ${appId}`);
 };
