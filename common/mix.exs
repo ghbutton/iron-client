04704e2b5b8c0530c01858409091f7f21269ed6c
Gary Button <garyhbutton@gmail.com>
2020-02-10 16:32:40 -0800

    Use elixir 1.6

--- common/mix.exs
+++ common/mix.exs
@@ -5,7 +5,7 @@ defmodule Common.MixProject do
     [
       app: :common,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
