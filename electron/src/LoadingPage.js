7a2b67cdf6770871b35288b65dee9eb5ab9127e9
Gary Button <garyhbutton@gmail.com>
2020-02-16 10:07:37 -0800

    Force users to update their name

--- electron/src/LoadingPage.js
+++ electron/src/LoadingPage.js
@@ -14,7 +14,12 @@ class LoadingPage extends Component {
   }
 
   async handleLoggedIn() {
-    this.props.history.push("/chats");
+    const currentUser = await window.controller.currentUser();
+    if (currentUser && (currentUser.attributes.name === "" || currentUser.attributes.name == null)) {
+      this.props.history.push("/user_edit_wizard");
+    } else {
+      this.props.history.push("/chats");
+    }
   }
 
   async componentDidMount() {
